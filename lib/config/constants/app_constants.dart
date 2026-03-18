bf9f39264da7f424a828f4190c2ab9b035dd1f69
Anush <anush.stha232@gmail.com>
2026-03-18 11:08:36 +0545

    add: constant and file structure

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConstants {
+  
+  // private constructor
+  AppConstants._();
+
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
+  static const String baseVersion = 'v1';
+  static const int connectTimeout = 10000;
+  static const int receiveTimeout = 10000;
+}
