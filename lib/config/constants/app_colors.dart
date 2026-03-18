d461daf3526a2145e45004f7cf04121245579347
Anush <anush.stha232@gmail.com>
2026-03-18 15:10:35 +0545

    add: colors constants

---
+++ lib/config/constants/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  // Primary brand color
+  static const primary       = Color.fromARGB(255, 108, 99, 255);
+  static const primaryDark   = Color.fromARGB(255, 75, 68, 204);
+  static const primaryLight  = Color.fromARGB(255, 238, 237, 254);
+
+  // Background shades
+  static const background    = Color.fromARGB(255, 15, 15, 19);
+  static const surface       = Color.fromARGB(255, 26, 26, 36);
+  static const surfaceAlt    = Color.fromARGB(255, 34, 34, 47);
+  static const card          = Color.fromARGB(255, 30, 30, 42);
+
+  // Text
+  static const textPrimary   = Color.fromARGB(255, 241, 239, 232);
+  static const textSecondary = Color.fromARGB(255, 153, 152, 160);
+  static const textHint      = Color.fromARGB(255, 92, 91, 100);
+
+  // Status
+  static const success       = Color.fromARGB(255, 29, 158, 117);
+  static const error         = Color.fromARGB(255, 226, 75, 74);
+  static const warning       = Color.fromARGB(255, 186, 117, 23);
+  static const info          = Color.fromARGB(255, 55, 138, 221);
+
+  // Border
+  static const border        = Color.fromARGB(255, 46, 46, 61);
+  static const borderLight   = Color.fromARGB(255, 58, 58, 77);
+}
