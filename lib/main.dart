48c2d7e1a5d6a5baccba951d1f0b5ca72e532d59
Anush <anush.stha232@gmail.com>
2026-03-19 00:22:15 +0545

    add: riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:portfolio_admin/app/app.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(MyApp());
+  runApp(const ProviderScope(child: App()));
 }
